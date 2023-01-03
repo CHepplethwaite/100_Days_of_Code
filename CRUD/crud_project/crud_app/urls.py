76dcb53407fb3a3c61a93e31c69994742c3d8f94
Clifford Hepplethwaite <cliffordrh@gmail.com>
2023-01-03 23:00:31 +0200

    Worked on CRUD views and URLs

--- CRUD/crud_project/crud_app/urls.py
+++ CRUD/crud_project/crud_app/urls.py
@@ -3,4 +3,6 @@ from . import views as crud_views
 
 urlpatterns = [
     path("",crud_views.CreateCBV.as_view(),name="create"),
+    path("Update/",crud_views.UpdateCBV.as_view,name="update"),
+    path("delete/",crud_views.DeleteCBV.as_view,name="delete"),
 ]
