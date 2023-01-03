117dfa204d9fb2e434d9a6a27228247e4416f4bb
Clifford Hepplethwaite <cliffordrh@gmail.com>
2023-01-03 06:07:23 +0200

    Resolved url reverse issue 	modified:   CRUD/crud_project/crud_app/urls.py 	modified:   CRUD/crud_project/crud_app/views.py

--- CRUD/crud_project/crud_app/urls.py
+++ CRUD/crud_project/crud_app/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views as crud_views
 
 urlpatterns = [
-    path("",crud_views.CreateView.as_view(),name="create"),
-
+    path("",crud_views.CreateCBV.as_view(),name="create"),
 ]
--- CRUD/crud_project/crud_app/views.py
+++ CRUD/crud_project/crud_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import CreateView,DeleteView
+from django.urls import reverse_lazy
 from .models import PatientInfo
 
 
 #def index(request):
  #   return render(request,"crud_app/index.html",{})
 
-class CreateCBV(CreateView):
+class CreateCBV(CreateView): # uses modelname_form.html form
     model=PatientInfo
     fields=["first_name",
     "last_name", 
@@ -17,4 +18,19 @@ class CreateCBV(CreateView):
     "image",
     "national_id"
     ]
+    success_url=reverse_lazy('create')
 
+class DeleteCBV():
+    model=PatientInfo
+
+class UpdateCBV():
+    model=PatientInfo
+    fields=["first_name",
+    "last_name", 
+    "age",
+    "phone_number",
+    "address",
+    "comment",
+    "image",
+    "national_id"
+    ]
