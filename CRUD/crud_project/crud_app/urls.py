0755778d6fa004e62fff47933ccf885f876734e0
Clifford Hepplethwaite <cliffordrh@gmail.com>
2023-01-10 16:45:48 +0200

    Consistent naming convention.

--- CRUD/crud_project/crud_app/urls.py
+++ CRUD/crud_project/crud_app/urls.py
@@ -4,7 +4,7 @@ from . import views as crud_views
 urlpatterns = [
     path("",crud_views.ListCBV.as_view(),name="list"),
     path("create/",crud_views.CreateCBV.as_view(),name="create"),
-    path("Update/",crud_views.UpdateCBV.as_view(),name="update"),
+    path("update/",crud_views.UpdateCBV.as_view(),name="update"),
     path("delete/",crud_views.DeleteCBV.as_view(),name="delete"),
     path("<pk>/",crud_views.DetailCBV.as_view(),name="detail"),
 ]
