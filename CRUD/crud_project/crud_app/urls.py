22225b56c50854029df6e969dd3ecde0aebc185a
Clifford Hepplethwaite <cliffordrh@gmail.com>
2023-01-10 11:46:31 +0200

    Worked on html templates.

--- CRUD/crud_project/crud_app/urls.py
+++ CRUD/crud_project/crud_app/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views as crud_views
 
 urlpatterns = [
-    path("",crud_views.CreateCBV.as_view(),name="create"),
+    path("",crud_views.ListCBV.as_view(),name="list"),
+    path("create/",crud_views.CreateCBV.as_view(),name="create"),
     path("Update/",crud_views.UpdateCBV.as_view(),name="update"),
     path("delete/",crud_views.DeleteCBV.as_view(),name="delete"),
-    path("list/",crud_views.ListCBV.as_view(),name="list"),
     path("<pk>/",crud_views.DetailCBV.as_view(),name="detail"),
 ]
